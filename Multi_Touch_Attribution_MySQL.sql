9919bac3bb25d609745bd85ac7bc3e89232ba54a
sakinasayed512 <sakinasayed512@gmail.com>
2026-07-28 14:40:49 +0530

    Spend table, CAC and ROAS by channel

--- Multi_Touch_Attribution_MySQL.sql
+++ Multi_Touch_Attribution_MySQL.sql
@@ -123,3 +123,77 @@ SELECT user_id FROM interactions WHERE conversion = 1
 WHERE touch_order = 1
 GROUP BY channel
 ORDER BY last_touch_conversions DESC;
+
+SELECT channel, COUNT(*) AS total_touchpoints
+FROM interactions
+GROUP BY channel
+ORDER BY total_touchpoints DESC;
+
+DROP TABLE channel_spend;
+
+CREATE TABLE channel_spend (
+    channel VARCHAR(50),
+    cpc DECIMAL(6,2),
+    total_touchpoints INT,
+    total_spend DECIMAL(10,2)
+);
+
+INSERT INTO channel_spend (channel, cpc, total_touchpoints, total_spend) VALUES
+('Search Ads', 25, 1609, 25*1609),
+('Display Ads', 8, 1669, 8*1669),
+('Social Media', 12, 1662, 12*1662),
+('Email', 2, 1654, 2*1654),
+('Referral', 5, 1685, 5*1685),
+('Direct Traffic', 0, 1721, 0);
+
+SELECT * FROM channel_spend;
+
+-- CAC per channel = Total Spend ÷ Attributed Conversions
+SELECT 
+    cs.channel,
+    cs.total_spend,
+    lw.total_attributed_conversions,
+    ROUND(cs.total_spend / lw.total_attributed_conversions, 2) AS CAC
+FROM channel_spend cs
+JOIN (
+    SELECT 
+        channel,
+        SUM(attribution_weight) AS total_attributed_conversions
+    FROM (
+        SELECT 
+            user_id,
+            channel,
+            1 / COUNT(*) OVER (PARTITION BY user_id) AS attribution_weight
+        FROM interactions
+        WHERE user_id IN (
+            SELECT user_id FROM interactions WHERE conversion = 1
+        )
+    ) AS linear_weights
+    GROUP BY channel
+) AS lw ON cs.channel = lw.channel
+ORDER BY CAC ASC;
+
+-- ROAS = (Attributed Conversions × AOV) ÷ Total Spend
+SELECT 
+    cs.channel,
+    cs.total_spend,
+    lw.total_attributed_conversions,
+    ROUND((lw.total_attributed_conversions * 500) / NULLIF(cs.total_spend, 0), 2) AS ROAS
+FROM channel_spend cs
+JOIN (
+    SELECT 
+        channel,
+        SUM(attribution_weight) AS total_attributed_conversions
+    FROM (
+        SELECT 
+            user_id,
+            channel,
+            1 / COUNT(*) OVER (PARTITION BY user_id) AS attribution_weight
+        FROM interactions
+        WHERE user_id IN (
+            SELECT user_id FROM interactions WHERE conversion = 1
+        )
+    ) AS linear_weights
+    GROUP BY channel
+) AS lw ON cs.channel = lw.channel
+ORDER BY ROAS DESC;
